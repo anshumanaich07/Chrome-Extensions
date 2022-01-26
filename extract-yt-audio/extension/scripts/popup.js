23ce48208752b4353133d8e9e2896c8332a25b9c
Anshuman Aich <anshuman.aich@bacancy.com>
2022-01-26 23:06:38 +0530

    refactored

--- extract-yt-audio/extension/scripts/popup.js
+++ extract-yt-audio/extension/scripts/popup.js
@@ -1,30 +1,22 @@
 var convert = $("#convert")
+var extractAudioDomain = "http://localhost:8000/extract-audio";
+var downloadAudioDomain = "http://localhost:8000/download-audio"; 
+
+async function extractAudio(domain, videoURL) {
+  var res = await fetch(domain, {method: "POST", body:  JSON.stringify({"videoURL": videoURL})});
+  var reader = res.body.getReader()
+  var res = await reader.read()
+  return res;
+}
 
 convert.on("click", function() {
   var videoURL = $("#ytURL").val()
-  console.log("received after button click: ", videoURL)
-  var extractAudioDomain = "http://localhost:8000/extract-audio";
-  var downloadAudioDomain = "http://localhost:8000/download-audio"; 
-  // var es = new EventSource("messages", {payload: {"ytURL":videoURL}});
-  fetch(extractAudioDomain, {
-    method: "POST",
-    body: JSON.stringify({"videoURL": videoURL})
-  }).then(function(res) {
-    console.log(res)
-    var es = new EventSource();
-    es.onmessage = function(e) {
-      console.log(e.Data)
-    }
-    // return res.json();
-  }).then(function(res) {
-    console.log("hi")
-    if (res.Msg === "download") {
-      // var es = new EventSource(downloadAudioDomain);
-      // es.onmessage = function(e) {
-      //   console.dir(e)
-      //   console.log(e.Progress)
-      // }
-      window.open(downloadAudioDomain);
-    };
-  });
+  console.log("received after button click: ", videoURL);
+
+  var res;
+  res = extractAudio(extractAudioDomain, videoURL);
+  res.then(function(res) {
+    var textDecoder = new TextDecoder();
+    console.log(textDecoder.decode(res.value))
+  })  
 })
